0e1cc429c85b1d4beffe8176d74d61e4be9593a0
Jack-Eddy <144264179+Jack-Eddy@users.noreply.github.com>
2023-10-10 15:36:30 -0400

    typescript version

--- main.ts
+++ main.ts
@@ -1,8 +1,52 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Jack Eddy
+ * Created on: Oct 2023
+ * This program plays rock paper scissors
 */
 
-basic.showString('Hello, World!')
+// variables
+let randomNumber: number
+let score: number
+score = 0
+
+// setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+// shake
+input.onGesture(Gesture.Shake, function () {
+  randomNumber = randint(0, 2)
+  basic.clearScreen()
+
+// rock
+  if (randomNumber == 0) {
+    basic.showIcon(IconNames.SmallSquare)
+  }
+
+// paper
+  if (randomNumber == 1) {
+    basic.showIcon(IconNames.Scissors)
+  }
+
+// paper
+  if (randomNumber == 2) {
+    basic.showIcon(IconNames.Square)
+  }
+
+// add point to score
+input.onButtonPressed(Button.A, function () {
+  score = score + 1
+  basic.showIcon(IconNames.Yes)
+})
+
+// show score
+input.onButtonPressed(Button.B, function () {
+  basic.showString("Score: " + (score).toString())
+})
+
+// pause
+basic.pause(5000)
+basic.showIcon(IconNames.Happy)
+})
